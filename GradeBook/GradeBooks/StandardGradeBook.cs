4e6131edc8c553dcd2283680695ed88a5d212970
Mateusz Kurowski <kurowskimateusz04@gmail.com>
2022-03-03 17:29:24 +0100

    Create a constructor for StandardGradeBook

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
+using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
     }
 }
