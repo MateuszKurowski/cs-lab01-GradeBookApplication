5f00b4ff77dac88ed729a3cfc1648ee286318ed1
Mateusz Kurowski <kurowskimateusz04@gmail.com>
2022-03-03 18:08:59 +0100

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -24,5 +24,13 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.Write("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
     }
 }
